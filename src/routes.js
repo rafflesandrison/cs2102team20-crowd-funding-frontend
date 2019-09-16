8f8f6c5818236891402dfdb65d1c1888b001fa04
jing1324 <jinjing123@gmail.com>
2019-09-16 12:50:52 +0800

    Cofigure vue router

---
+++ src/routes.js
@@ -0,0 +1,9 @@
+import Home from "./components/Home.vue";
+import Register from "./components/Register.vue";
+
+const routes = [
+  { path: "/", component: Home },
+  { path: "/register", component: Register }
+];
+
+export default routes;
