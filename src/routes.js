e048a4d5487d9e5282bac6564470451a13f2ce17
Ian Wu <iwle@u.nus.edu>
2019-09-26 17:15:52 +0800

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,12 @@
 import Home from "./components/Home.vue";
+import Login from "./components/Login.vue";
 import Register from "./components/Register.vue";
 
 const routes = [
-  { path: "/", component: Home },
-  { path: "/register", component: Register }
+  { path: "/", redirect: '/login' },
+  { path: "/register", component: Register },
+  { path: "/home", component: Home },
+  { path: "/login", component: Login }
 ];
 
 export default routes;
