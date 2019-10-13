639422011d68785390c9f015acf96d4d442b9d35
Ian Wu <iwle@u.nus.edu>
2019-10-14 00:49:41 +0800

    Update main.js Include new Vuex store in app.

--- src/main.js
+++ src/main.js
@@ -2,19 +2,28 @@ import Vue from "vue";
 import App from "./App.vue";
 import VueRouter from "vue-router";
 import routes from "./routes";
-import BootstrapVue from 'bootstrap-vue'
+import BootstrapVue from 'bootstrap-vue';
 
-Vue.use(BootstrapVue)
+import Vuex from 'vuex';
 
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
+Vue.use(BootstrapVue);
+Vue.use(Vuex);
+
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
 
 Vue.use(VueRouter);
 Vue.config.productionTip = false;
 
 const router = new VueRouter({ routes });
 
+const store = new Vuex.Store({
+  state: { count: 0, },
+  mutations: { }
+})
+
 new Vue({
   router,
+  store,
   render: h => h(App)
 }).$mount("#app");
