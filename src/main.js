7efba21a1c1100d8d3d2aa4ecd5105d0c8e7b355
Ian Wu <iwle@u.nus.edu>
2019-10-24 22:06:53 +0800

    Update main.js Fix some minor issues.

--- src/main.js
+++ src/main.js
@@ -6,12 +6,11 @@ import BootstrapVue from 'bootstrap-vue';
 
 import Vuex from 'vuex';
 
-Vue.use(BootstrapVue);
-Vue.use(Vuex);
-
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
 
+Vue.use(BootstrapVue);
+Vue.use(Vuex);
 Vue.use(VueRouter);
 Vue.config.productionTip = false;
 
