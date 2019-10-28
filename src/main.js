9fa28c1b8d5f05f82d74eefe1a55d86baa96ad14
Ian Wu <iwle@u.nus.edu>
2019-10-28 13:45:15 +0800

    Update main.js Include 'element-ui' and 'vue-data-tables' as imports.

--- src/main.js
+++ src/main.js
@@ -3,17 +3,29 @@ import App from "./App.vue";
 import VueRouter from "vue-router";
 import routes from "./routes";
 import BootstrapVue from 'bootstrap-vue';
+
 import '@fortawesome/fontawesome-free/css/all.css';
 import '@fortawesome/fontawesome-free/js/all.js';
 
 import Vuex from 'vuex';
+import ElementUi from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css'
 
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
 
+import VueDataTables from 'vue-data-tables'
+
+import lang from 'element-ui/lib/locale/lang/en'
+import locale from 'element-ui/lib/locale'
+
+locale.use(lang)
+
+Vue.use(VueDataTables)
 Vue.use(BootstrapVue);
 Vue.use(Vuex);
 Vue.use(VueRouter);
+Vue.use(ElementUi)
 Vue.config.productionTip = false;
 
 const router = new VueRouter({ routes });
